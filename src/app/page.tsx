614fd53c73163c95cd5bc81778f2f1ba55950f8d
Alihamxa01 <hamza.nadeem.0106@gmail.com>
2025-01-05 22:50:56 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default async function Page() {
       blogs.map((blog, index) => (
         <div key={index}>
 <h1 className="text-5xl text-green-500 font-black"><span className="text-blue-400 text-xl">publish by</span>{blog.title}</h1>
-<p className='text-2xl underline'><span className="text-orange-500 text-xl">DISCRIPTION is writen below</span><br/>{blog.name}</p>
+<p className="text-2xl underline"><span className="text-orange-500 text-xl">DISCRIPTION is writen below</span><br/>{blog.name}</p>
 <p>{blog.details}</p>
 
         </div>
